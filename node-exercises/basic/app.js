73b8cf59a4762a136cf19e5be6922b195b268d1d
Eude Lesperance <l.keude96@gmail.com>
2017-04-23 20:32:40 -0500

    Modified the hello world text and added comments

--- node-exercises/basic/app.js
+++ node-exercises/basic/app.js
@@ -1,6 +1,10 @@
+// require is kinda like import, specially for libraries
 var http = require('http');
+
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(8080, '127.0.0.1');
+
 console.log('Server running at http://127.0.0.1:8080/');
